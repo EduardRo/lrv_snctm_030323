698aa9cc266b35724fa92494a4e529afa4b5cfa7
EduardRo <eduardstancu@yahoo.com>
2023-03-03 13:06:55 +0200

    product model controller creation

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
+//use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/products', [ProductController::class, 'index']
+);
+
+//Route::post('/products', );
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
