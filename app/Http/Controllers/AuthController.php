64dbaff218658754faab45f8cba4a2d9e506f31e
EduardRo <eduardstancu@yahoo.com>
2023-03-10 11:56:02 +0200

    finished

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 //use Illuminate\Http\Response;
-//use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 //use Laravel\Sanctum\HasApiTokens;
 //use Illuminate\Support\Facades\Auth;
@@ -25,6 +25,32 @@ class AuthController extends Controller
             'email'=>$fields['email'],
             'password'=>bcrypt($fields['password'])
         ]);
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user'=>$user,
+            'token'=>$token
+        ];
+
+        return response($response, 201);
+
+    }
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email'=>'required|email',
+            'password'=>'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+        // Check password
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message'=>'Bad credential'
+            ], 401);
+        } 
 
         $token = $user->createToken('myapptoken')->plainTextToken;
 
--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout',[AuthController::class, 'logout']);
 
 });
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register',[AuthController::class, 'register']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}',[ProductController::class,'show']);
